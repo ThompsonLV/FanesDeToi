16ecf623d41f1d2e7fe4258a22e99a13bb55fca8
el azhari tifour <lazari.pro@gmail.com>
2023-08-22 14:05:20 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!( email: "lazbotcod@gmail.com",
-              encrypted_password: "10254785",
-              name: "lazari",
-              iban: "5555")
 
-User.create!(email: "blabla@gmail.com",
-             encrypted_password: "123456",
-             name: "Cristiano",
-             iban: "6982")
-
-User.create!(email: "irieeeen@gmail.com",
-             encrypted_password: "78987854e",
-             name: "Eikichi",
-             iban: "5555")
