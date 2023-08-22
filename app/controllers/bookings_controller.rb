8a456e65a59778844bd287053699d07705e9ea31
el azhari tifour <lazari.pro@gmail.com>
2023-08-22 14:58:25 +0200

    vues et booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,23 @@
 class BookingsController < ApplicationController
   def new
-      @fanes = Fanes.find(params[:fane_id])
-      @lessor = booking.user
+    @fane = Fane.find(params[:fane_id])
+    @user = User.find(params[:user_id])
+    @owner = Booking.fane.user
+    @lessor = Booking.user
+    @booking = Booking.new(booking_params)
   end
 
-  def create
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ User.destroy_all
 @thomas = User.create!(email: "thomas@gmail.com", name: "Thomas", iban: "1234", password: 'password123', password_confirmation: 'password123')
 User.create!(email: "charles@gmail.com", name: "charles", iban: "4321", password: 'password123', password_confirmation: 'password123')
 User.create!(email: "Azhari@gmail.com", name: "Azhari", iban: "2413", password: 'password123', password_confirmation: 'password123')
-
+puts "t'es a la 1ere etape"
 @fane = Fane.create!(title: "mongomeri", brand: "Dyson", start_date: "22/04/2000", end_date: "24/04/2000", validation: nil, user_id: @thomas.id)
 Fane.create!(title: "pirouette", brand: "Valseuse", start_date: "22/03/2010", end_date: "31/04/2020", validation: nil, user_id: @thomas.id)
-
+puts 
 Booking.create!(start_date: "17/03/2015", end_date: "04/04/2023", user_id: @thomas.id, fane_id: @fane.id)
 Booking.create!(start_date: "13/03/1995", end_date: "04/04/2022", user_id: @thomas.id, fane_id: @fane.id)
