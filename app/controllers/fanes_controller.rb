72dcede79c888f24f6c4ec704a376b90ea045fcd
Thomas Le Veo <thomasleveo@gmail.com>
2023-08-24 15:47:04 +0200

    Price done

--- app/controllers/fanes_controller.rb
+++ app/controllers/fanes_controller.rb
@@ -69,6 +69,6 @@ class FanesController < ApplicationController
   end
 
   def fane_params
-    params.require(:fane).permit(:photos, :title, :brand, :start_date, :end_date, photos: [])
+    params.require(:fane).permit(:photos, :title, :brand, :start_date, :end_date, :price_per_day, photos: [])
   end
 end
